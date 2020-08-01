8d693e19692db645c7dfefa618cb7b3700fc58a0
Karen Ren <466471299@qq.com>
2020-08-01 17:01:33 +0800

    homework for lession 04

--- lession04.py
+++ lession04.py
@@ -1,4 +1,5 @@
 # trim()函数实现
+print("trim()函数实现")
 def left_trim(before_trim):
     for i, j in enumerate(before_trim):
         if j != " ":
@@ -18,4 +19,55 @@ def trim_function(before_trim):
 before_trim = " 6789 dsjfjdskl     "
 print(trim_function(before_trim))
 
+# datetime计算时间差
+print("-------------------------------------------------------------------------")
+print("datetime计算时间差")
+from datetime import datetime
+
+
+def friend_datetime(timestamp):
+    current_time = datetime.now()
+    print(datetime.strftime(current_time, "Current time: %Y-%m-%d at %H:%M %p"))
+    print(datetime.strftime(timestamp, "Input time: %Y-%m-%d at %H:%M %p"))
+    time_in_seconds = (current_time - timestamp).total_seconds()
+    mins = int(time_in_seconds//60)
+    hours = int(time_in_seconds//3600)
+    days = (current_time - timestamp).days
+    if days > 1:
+        print(datetime.strftime(timestamp, str(days) + " days ago at %H:%M %p"))
+    elif days < 1 and hours > 12:
+        print(datetime.strftime(timestamp, " Today at %H:%M %p"))
+    elif days < 1 and hours > 1 and hours < 12:
+        print(datetime.strftime(timestamp, str(hours) + " hours ago at %H:%M %p"))
+    elif days < 1 and hours < 1:
+        print(datetime.strftime(timestamp, str(mins) + " minutes ago at %H:%M %p"))
+
+
+timestamp = datetime(2020, 7, 1, 15, 19, 12)
+friend_datetime(timestamp)
+print("\n")
+timestamp = datetime(2020, 8, 1, 1, 19, 12)
+friend_datetime(timestamp)
+print("\n")
+timestamp = datetime(2020, 8, 1, 10, 19, 12)
+friend_datetime(timestamp)
+print("\n")
+timestamp = datetime(2020, 8, 1, 15, 19, 12)
+friend_datetime(timestamp)
+
+print("-------------------------------------------------------------------------")
+print("杨辉三角 生成器实现")
+def generator():
+    N = [1]
+    while True:
+        yield N
+        S = N[:]
+        S.append(0)
+        N = [S[i-1] + S[i] for i in range(len(S))]
+
+
+T = generator()
+for i in range(10):
+    print(next(T))
+    i = i + 1
 
