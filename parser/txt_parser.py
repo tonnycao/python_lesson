d7c882066fb4bd97f25f12ab33d1b12388682a7f
caojian <647812411@qq.com>
2020-08-15 09:06:12 +0800

    update txt parser

--- parser/txt_parser.py
+++ parser/txt_parser.py
@@ -4,6 +4,8 @@ from .parser import Parser
 
 
 class TxtParser(Parser):
+    count = 0
+    offset = None
 
     def __init__(self, path, delimiter):
         self.path = path
@@ -26,17 +28,30 @@ class TxtParser(Parser):
         return data
 
     def chunk_reader(self, max_rows) -> List:
-        pass
+        data = []
+        with open(self.path) as f:
+            if TxtParser.offset:
+                f.seek(TxtParser.offset)
+            row = 0
+            while row < max_rows-1:
+                row += 1
+                row_data = f.readline()
+                if not row_data:
+                    break
+                data.append(row_data)
+                if row == max_rows:
+                    TxtParser.offset = f.tell()
+                    break
 
+        TxtParser.count += 1
+
+        return data
+
+    @staticmethod
     def read_in_chunks(file_object, chunk_size=1024):
-        """Lazy function (generator) to read a file piece by piece.
-        Default chunk size: 1k."""
         while True:
             data = file_object.read(chunk_size)
             if not data:
                 break
             yield data
 
-    with open('really_big_file.dat') as f:
-        for piece in read_in_chunks(f):
-            pass
